715bddcd18dc788f4f622e4dd03315bab1054822
phreej $ voo <phreej2voo@gmail.com>
2024-05-23 17:35:44 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,57 @@ fn main() {
     
     // 元祖  tuple  是用一对()包括的一组数组
     let _tup: (i32, f64, u8) = (500, 6.4, 1);
-    // let (x, y, z) = tup;
+    let (_x, _y, _z) = _tup;
 
     // 数组  使用一对[]包括的同类型数据
     let _a = [1, 2, 3, 4, 5];
     // let c: [i32; 5];
 
     println!("{}", add(2, 3));
+
+    // 条件语句
+    let mut number = if a > 0 {1} else {-1};
+    println!("numebr is {}", number);
+
+
+    // 循环
+    while number != 4 {
+        println!("{}", number);
+        number += 1;
+    }
+    println!("EXIT");
+
+    let a = [10, 20, 30, 40, 50];
+    for i in a.iter() {
+        println!("the value is: {}", i);
+    }
+
+    for i in 0..5 {
+        println!("a[{}] = {}", i, a[i]);
+    }
+
+    let s = ['p', 'h', 'r', 'e', 'e', 'j'];
+    let mut i = 0;
+    loop {
+        let ch = s[i];
+        if ch == 'j' {
+            break;
+        }
+        println!("\'{}\'", ch);
+        i += 1;
+    }
+
+    let location = loop {
+        let ch = s[i];
+        if ch == 'j' {
+            break i;
+        }
+        i += 1;
+    };
+    println!("\'j\'的索引为 {}", location);
+
+    // 迭代器
+    
 }
 
 // function add method
@@ -25,3 +69,5 @@ fn main() {
 fn add(a: i32, b: i32) -> i32{
     return a + b;
 }
+
+//Rust中的条件表达式必须是bool类型
